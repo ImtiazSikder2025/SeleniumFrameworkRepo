6074e6be8ea63dc6b27fb0df4c68b806b373076d
Imtiaz <imti@gmail.com>
2025-04-08 21:39:14 -0400

    TC_RF_005 is added

--- src/test/java/com/register/TC_RF_005.java
+++ src/test/java/com/register/TC_RF_005.java
@@ -10,15 +10,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.utils.CommonUtilities;
-//Register functionalities
-public class TC_RF_001 {
-	
-	WebDriver driver;
+
+public class TC_RF_005 {
+
+WebDriver driver;
 	
 	@BeforeMethod
 	public void setup() throws Exception {
@@ -58,11 +57,11 @@ public class TC_RF_001 {
 	
 	
 	
+	
+	//Verify Registering an account when 'Yes' option is selected for NewsLetter field
 	@Test
-	public void verifyRegisteringByProvidingOnlyMandatoryFields() throws Exception {
-		// Verify Registering an account by providing only mandatory fields	
+	public void verifyYesLetterIsSelectedForNewsLetter() {
 		
-		//Mandatory fields
 		driver.findElement(By.name("firstname")).sendKeys("Bret");
 		driver.findElement(By.name("lastname")).sendKeys("Hart");
 		driver.findElement(By.name("email")).sendKeys(CommonUtilities.generateBrandNewEmail());
@@ -70,57 +69,31 @@ public class TC_RF_001 {
 		driver.findElement(By.name("password")).sendKeys("pppppp");
 		driver.findElement(By.name("confirm")).sendKeys("pppppp");
 		driver.findElement(By.name("agree")).click();
+		//Clicking on 'Yes' option for NewsLetter
+		driver.findElement(By.xpath("//input[@name='newsletter' and @value='1']")).click();
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
 		
-		
-		
-		//Verification 
 		String expectedText="Your Account Has Been Created!";
 		String actualText=driver.findElement(By.xpath("//h1[text()='Your Account Has Been Created!']")).getText();
 		Assert.assertEquals(actualText, expectedText);
 		
-		boolean link=driver.findElement(By.linkText("Logout")).isDisplayed();
-		Assert.assertTrue(link);		
+		boolean flag=driver.findElement(By.xpath("//a[text()='Success']")).isDisplayed();
+		Assert.assertTrue(flag);
 		
-		String actualTitle=driver.getTitle();
-		String expectedTitle="Your Account Has Been Created!";
-		Assert.assertEquals(actualTitle,expectedTitle);		
+		//Clicking on 'Continue' button
+		driver.findElement(By.xpath("//a[text()='Continue']")).click();		
+		//Verify User should be taken to Account page
+		String expectedHeading="My Account";
+		String actualHeading=driver.findElement(By.xpath("//h2[text()='My Account']")).getText();
+		Assert.assertEquals(actualHeading, expectedHeading);
 		
-		String actualDetails=driver.findElement(By.id("content")).getText();
-		String expectedDetailsOne="Congratulations! Your new account has been successfully created!";
-		String expectedDetailsTwo="You can now take advantage of member privileges to enhance your online shopping experience with us.";
-		String expectedDetailsThree="If you have ANY questions about the operation of this online shop, please e-mail the store owner.";
-		String expectedDetailsFour="A confirmation has been sent to the provided e-mail address. If you have not received it within the hour, please contact us.";
-		Assert.assertTrue(actualDetails.contains(expectedDetailsOne));
-		Assert.assertTrue(actualDetails.contains(expectedDetailsTwo));
-		Assert.assertTrue(actualDetails.contains(expectedDetailsThree));
-		Assert.assertTrue(actualDetails.contains(expectedDetailsFour));
+		//Clicking on 'Subscribe / unsubscribe' option
+		driver.findElement(By.xpath("//a[text()='Subscribe / unsubscribe to newsletter']")).click();
+		//Verify 'Yes' option is selected by default in the NewsLetter page
+		boolean yesSelected=driver.findElement(By.xpath("//input[@name='newsletter' and @value='1']")).isSelected();
+		Assert.assertTrue(yesSelected);
 		
-		
-		//Account page		
-		driver.findElement(By.linkText("Continue")).click();
-		boolean expectedDetails=driver.findElement(By.linkText("Edit your account information")).isDisplayed();
-		Assert.assertTrue(expectedDetails);		
+	
 	}
-
-	@AfterMethod
-	public void teardown() {
-		if(driver!=null) {
-			driver.quit();
-		}
-	}
-
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
